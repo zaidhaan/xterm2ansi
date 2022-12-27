61a1f598a5c04f8377f6a314598391c875c1b883
Zaidhaan Hussain <zaidhaan03@gmail.com>
2022-12-27 13:52:19 +0800

    remove unused declarations

--- xterm2ansi.h
+++ xterm2ansi.h
@@ -14,9 +14,6 @@ typedef struct Color {
 
 static const Color ansi_colors[16];
 
-int get_control_sequence(char *input);
-void handle_control_sequence(char *escape_code);
-
 int xterm_to_ansi(int color);
 void xterm_to_rgb(int color, int *r, int *g, int *b);
 double color_distance(int r1, int g1, int b1, int r2, int g2, int b2);
