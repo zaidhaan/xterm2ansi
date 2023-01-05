802280f2aafef78839f145658b6d7f8af0207ea3
Zaidhaan Hussain <zaidhaan03@gmail.com>
2023-01-05 08:43:38 +0800

    refactor: use state machine approach + true color
    This refactor has each character read in sequence while building a
    struct representing the CSI, once built, it gets processed (buffer to
    argument array) and converted to ansi, then finally printed.
    
    Adds more code but it's much more maintainable and extensible (supports
    24-bit "true color" now very easily)
    
    Test case for highlighting had to be updated to convert ^[[m to ^[[0m
    as 0 should be treated as a valid argument, if not 0s in 24-bit color
    would be hard to implement

--- xterm2ansi.c
+++ xterm2ansi.c
@@ -3,46 +3,125 @@
 #include <string.h>
 #include <math.h>
 #include "xterm2ansi.h"
-#include "utils.h"
 
-void process_escape_code(char *escape_code) {
-    int i = 0;
-    int fg_set = 0;
-    int bg_set = 0;
-    int prev = 0;
-    printf("\033[");
-    while (i < strlen(escape_code)) {
-        char num[4] = { 0 };
-        int j = 0;
-        char last = 0;
-        while (escape_code[i] != ';' && i < strlen(escape_code)) {
-            num[j++] = escape_code[i++];
+#define CSI_MAX_ARGS 16
+#define CSI_MAX_LEN  64
+
+typedef struct {
+    int args [CSI_MAX_ARGS];
+    int num_args;
+    char buffer[CSI_MAX_LEN];
+    int len;
+} CSI_code;
+
+enum state {
+    STATE_NORMAL,
+    STATE_ESCAPE,
+    STATE_CSI,
+};
+
+void csi_conv_xterm_ansi(CSI_code csi_in, CSI_code *csi_out) {
+    int color;
+    int diff = 0;
+
+    for (int i = 0; i < csi_in.num_args; i++) {
+        switch (csi_in.args[i]) {
+            case 48: /* background */
+                diff = 10;
+            case 38: /* foreground */
+                if (csi_in.args[i + 1] == 5) {
+                    color = xterm_to_ansi(csi_in.args[i + 2]);
+                    csi_out->args[csi_out->num_args++] = color + diff;
+                    i += 2;
+                } else if (csi_in.args[i + 1] == 2) {
+                    color = rgb_to_ansi(csi_in.args[i + 2],
+                                        csi_in.args[i + 3],
+                                        csi_in.args[i + 4]);
+                    csi_out->args[csi_out->num_args++] = color + diff;
+                    i += 4;
+                }
+                break;
+            default:
+                csi_out->args[csi_out->num_args++] = csi_in.args[i];
+                break;
         }
-        if (i < strlen(escape_code)) {
-            last = escape_code[i];
-        }
-        int code = atoi(num);
-        if (fg_set) {
-            printf("%d", xterm_to_ansi(code));
-            if (last) printf("%c", last);
-        } else if (bg_set) {
-            printf("%d", xterm_to_ansi(code) + 10);
-            if (last) printf("%c", last);
-        } else if (code != 38 && code != 48 && prev != 38 && prev != 48) {
-            printf("%d", code);
-            if (last) printf("%c", last);
-        }
-        fg_set = (prev == 38 && code == 5);
-        bg_set = (prev == 48 && code == 5);
-        prev = code;
-        i++;
     }
-    printf("m");
+}
+
+void parse_csi(CSI_code *csi) {
+    char *cur = csi->buffer;
+    char *end = NULL;
+    long int num;
+    while (cur < csi->buffer + csi->len) {
+        num = strtol(cur, &end, 10);
+        if (end == cur) {
+            num = 0;
+        }
+        cur = end;
+        csi->args[csi->num_args++] = num;
+        if (csi->num_args >= CSI_MAX_ARGS) {
+            break;
+        }
+        cur++;
+    }
+}
+
+void csi_print(CSI_code csi, FILE *stream) {
+    fputc('\033', stream);
+    fputc('[', stream);
+    for (int i = 0; i < csi.num_args; i++) {
+        fprintf(stream, "%d", csi.args[i]);
+        if (i < csi.num_args - 1) {
+            fputc(';', stream);
+        }
+    }
+    fputc('m', stream);
+}
+
+void put_conv_chr(char c, FILE *stream) {
+    static enum state state = STATE_NORMAL;
+    static CSI_code csi_in, csi_out;
+
+    switch (state) {
+        case STATE_NORMAL:
+            if (c == '\033') {
+                state = STATE_ESCAPE;
+            } else {
+                fputc(c, stream);
+            }
+            break;
+        case STATE_ESCAPE:
+            if (c == '[') {
+                state = STATE_CSI;
+                csi_in.num_args = 0;
+                csi_in.len = 0;
+            } else {
+                state = STATE_NORMAL;
+                fputc('\033', stream);
+                fputc(c, stream);
+            }
+            break;
+        case STATE_CSI:
+            csi_in.buffer[csi_in.len++] = c;
+
+            // NOTE: probably odd to process on max length?
+            if (c == 'm' || csi_in.len >= CSI_MAX_LEN) {
+                state = STATE_NORMAL;
+
+                csi_in.buffer[csi_in.len] = '\0';
+                parse_csi(&csi_in);
+
+                csi_out.num_args = 0;
+                csi_out.len = 0;
+                csi_conv_xterm_ansi(csi_in, &csi_out);
+                csi_print(csi_out, stream);
+            }
+            break;
+    }
 }
 
 int main(int argc, char *argv[]) {
     FILE *input;
-    char line[MAX_LINE_LENGTH];
 
     if (argc > 1) {
         input = fopen(argv[1], "r");
@@ -54,29 +133,10 @@ int main(int argc, char *argv[]) {
         input = stdin;
     }
 
-    while (fgets(line, MAX_LINE_LENGTH, input)) {
-        for (int i = 0; i < strlen(line); i++) {
-            if (line[i] == '\033' && line[i + 1] == '[') {
-                char *csi_end = line + i + 2;
-                char *m_pos = strchr(csi_end, 'm');
-
-                if (m_pos != NULL) {
-                    char escape_code[MAX_ESCAPE_CODE_LENGTH];
-                    int ec_len = m_pos - csi_end;
-                    strncpy(escape_code, m_pos - ec_len, ec_len);
-                    escape_code[ec_len] = '\0';
-
-                    process_escape_code(escape_code);
-
-                    i = m_pos - line;
-                }
-            } else {
-                fputc(line[i], stdout);
-            }
-        }
+    char c;
+    while ((c = fgetc(input)) != EOF) {
+        put_conv_chr(c, stdout);
     }
-
-    fclose(input);
 
     return EXIT_SUCCESS;
 }
